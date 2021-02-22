d805ba8d2793675ca1520c5697dd97ba81b03b30
xingxiaohui <1559237979@qq.com>
2021-02-22 18:01:24 +0800

    整合swagger

--- service/service-edu/src/main/java/com/xxh/eduservice/EduServiceApplication.java
+++ service/service-edu/src/main/java/com/xxh/eduservice/EduServiceApplication.java
@@ -2,8 +2,10 @@ package com.xxh.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.xxh")
 public class EduServiceApplication {
     public static void main(String[] args){
         SpringApplication.run(EduServiceApplication.class,args);
