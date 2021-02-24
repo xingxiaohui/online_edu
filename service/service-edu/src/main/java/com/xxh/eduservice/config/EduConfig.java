c4a6f13ff832046b2ed8295e915f1fb1073f319f
xingxiaohui <1559237979@qq.com>
2021-02-24 20:24:29 +0800

    分页配置

--- service/service-edu/src/main/java/com/xxh/eduservice/config/EduConfig.java
+++ service/service-edu/src/main/java/com/xxh/eduservice/config/EduConfig.java
@@ -1,9 +1,20 @@
 package com.xxh.eduservice.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @MapperScan("com.xxh.eduservice.mapper")
 public class EduConfig {
+    // 最新版配置 mybatis-plus 分页插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
 }
